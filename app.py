e823a4504a821f7ca0862ed863408af609133f0a
Jacob <jacob.maresca@googlemail.com>
2016-03-11 17:03:27 +0000

    made pretty charts

--- app.py
+++ app.py
@@ -123,8 +123,9 @@ def hello():
         zpos_data = zpos_data/400000
         rpos = zip(xpos_data,ypos_data,zpos_data)
 
-        box_data, hist, bin = histogramGen(dx,dy,dz,100,rpos)
-        return json.dumps([hist.tolist(),bin.tolist()])
+        box_data, hist, bin = histogramGen(dx,dy,dz,500,rpos)
+        #return json.dumps([hist.tolist(),bin.tolist()])
+        return '<!doctype html><html><head><title>Bar Chart</title><script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script></head><body><h1>Galaxy Density</h1><p>'+json.dumps([hist.tolist(),bin.tolist()])+'</p><div style="width: 100%"><canvas id="canvas" height="450" width="1000"></canvas></div><script>var randomScalingFactor = function(){ return Math.round(Math.random()*100)};var barChartData = {labels : '+json.dumps(bin.tolist())+',datasets : [{fillColor : "rgba(220,220,220,0.5)",strokeColor : "rgba(220,220,220,0.8)",highlightFill: "rgba(220,220,220,0.75)",highlightStroke: "rgba(220,220,220,1)",data : '+json.dumps(hist.tolist())+'}]};window.onload = function(){var ctx = document.getElementById("canvas").getContext("2d");window.myBar = new Chart(ctx).Bar(barChartData, {responsive : true });}</script></body></html>'
 
     else:
         return "<h1>Welcome to GalaxyQuest</h1> <h3>Use:<h3><ul><li>http://galaxyquest.herokuapp.com?xpos=0&ypos=0&zpos=0&dx=0.1&dy=0.1&dz=0.1</li></ul>"    
